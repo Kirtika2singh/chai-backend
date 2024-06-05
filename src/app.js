593f48ee91f04f545abf2a7a88e14acd2df7211e
Kirtika2singh <136922844+Kirtika2singh@users.noreply.github.com>
2024-06-05 14:26:10 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser"; //cookie parser is a middleware which parses the cookies from the request headers and populates the req.cookies object with the cookie name-value pairs
-import cors from "cors";
+import cors from "cors"; 
 
 const app = express();
 
@@ -17,9 +17,19 @@ app.use(express.json({
 }));
 
 app.use(express.urlencoded({extended: true, limit: "16kb"})); //express.urlencoded() is a middleware that parses incoming requests with urlencoded payloads and is based on body-parser 
+
 app.use(express.static("public")); //express.static() is a middleware that serves static files like images,favicon
 
-app.use(express.static("public")) //express.static() is a middleware that serves static files like images,favicon
-
 app.use(cookieParser()); //cookie parser is a middleware which parses the cookies from the request headers and populates the req.cookies object with the cookie name-value pairs
-export default app;
+
+//Routes import
+
+import userRouter from "./routes/user.route.js";
+
+//routes declaration
+
+app.use("/api/v1/users" , userRouter); //userRouter is a middleware that handles all the routes starting with /user
+
+//http://localhost:8000/api/v1/users/register
+
+export {app}; //exporting the app instance to be used in other files
